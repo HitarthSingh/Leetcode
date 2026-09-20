c96717046be786f75ce0ce62f7d6dfd313539479
Hitarth  Singh <hitarth05052006@gmail.com>
2026-09-20 12:02:02 +0530

    Time: 0 ms (100%), Space: 9.9 MB (15.78%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int value = 26 - (s[i] - 'a');
+            sum += (i + 1) * value;
+        }
+        return sum;
+    }
+};
